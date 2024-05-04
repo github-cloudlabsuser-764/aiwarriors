73b76d0a0ee6c7f7c81b916113a19e3a19a6cc6c
github-cloudlabsuser-764 <167455164+github-cloudlabsuser-764@users.noreply.github.com>
2024-05-04 21:50:33 +0530

    Update Transaction.cs

--- AI.StoreOperations/Model/Transaction.cs
+++ AI.StoreOperations/Model/Transaction.cs
@@ -18,5 +18,6 @@ namespace AI.StoreOperations.Model
         public string Category { get; set; }
         public string Product { get; set; }
         public double Amount { get; set; }
+        public double SalesVolume { get; set; }
     }
 }
